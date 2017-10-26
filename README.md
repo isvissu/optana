40ac96f2fa829ca1c5d18b87ff0215066f8b8d2f
viswateja Gajulavarthy <isvissu@gmail.com>
2017-10-26 13:00:09 +0530

    front end of the project

--- README.md
+++ README.md
@@ -1,9 +1,106 @@
-#optana
-here we work on the cutting edge technologies relating to the map.
-our intreast is on developing cutting edge software and hardware on the map concepts
-for examples
-geo-locating 
-geo-encoding 
-geo-decoding
+# hapi-validation-question
 
-opt + ana => optimization of the flora and fuana 
+Hapi.js Validation with Joi + `failAction` question.
+
+## Situation
+
+We want to build a "*traditional*" **server-side**-***only***
+rendered application using **Hapi**.
+
+While trying to *understand* how to avoid returning a "*raw*" `400`
+error to the client when `Joi` validation *fails*:
+
+![register-iphone4s-sim](https://cloud.githubusercontent.com/assets/194400/10234331/a863744a-688b-11e5-9eb8-5e41d0f570e2.png)
+
+We want to intercept the "*email not allowed to be empty*" (*Joi*)
+**validation error** and *instead* display the *error message*
+in the ***html template*** to the client,
+rather than returning the `400` error.
+
+[@AdriVanHoudt](https://github.com/hapijs/joi/issues/725#issuecomment-144482794) advised that we should:
+> "Look at `failAction` under http://hapijs.com/api#route-options "
+
+And [@MattHarrison](https://github.com/hapijs/joi/issues/725#issuecomment-144867144) elaborated that the `failAction` should be a function.
+
+---
+
+## Solution
+
+[![Build Status](https://travis-ci.org/nelsonic/hapi-validation-question.svg)](https://travis-ci.org/nelsonic/hapi-validation-question)
+[![codecov.io](http://codecov.io/github/nelsonic/hapi-validation-question/coverage.svg?branch=master)](http://codecov.io/github/nelsonic/hapi-validation-question?branch=master)
+[![Code Climate](https://codeclimate.com/github/nelsonic/hapi-validation-question/badges/gpa.svg)](https://codeclimate.com/github/nelsonic/hapi-validation-question)
+[![HitCount](https://hitt.herokuapp.com/nelsonic/hapi-validation-question.svg)](https://github.com/nelsonic/hapi-validation-question)
+
+
+We added `failAction` which ***re-uses*** the `register_handler`
+so that the `registration-form.html` is shown with any input validation error message (*until it is submitted with valid data*)
+
+```js
+{
+  method: '*',
+  path: '/register',
+  config: {
+    validate: {
+      payload : register_fields,
+      failAction: register_handler // register_handler is dual-purpose (see below!)
+    }
+  },
+  handler: register_handler
+}
+```
+
+the `register_handler` is:
+
+```js
+function register_handler(request, reply, source, error) {
+  // show the registration form until its submitted correctly
+  if(!request.payload || request.payload && error) {
+    var errors, values; // return empty if not set.
+    if(error && error.data) { // means the handler is dual-purpose
+      errors = extract_validation_error(error); // the error field + message
+      values = return_form_input_values(error); // avoid wiping form data
+    }
+    return reply.view('registration-form', {
+      title  : 'Please Register ' + request.server.version,
+      error  : errors, // error object used in html template
+      values : values  // (escaped) values displayed in form inputs
+    }).code(error ? 400 : 200); // HTTP status code depending on error
+  }
+  else { // once successful, show welcome message!
+    return reply.view('welcome-message', {
+      name   : validator.escape(request.payload.name),
+      email  : validator.escape(request.payload.email)
+    })
+  }
+}
+```
+> See: [**server.js**:57](https://github.com/nelsonic/hapi-validation-question/blob/master/server.js#L57) for complete file.
+
+Where `extract_validation_error(error)` and `return_form_input_values(error)`
+are helper functions defined within `server.js` (*but would be split out into re-useable view helpers*) which keep our handler function lean.
+
+When we submit the form without any of the required fields we see:
+
+![register-1of4](https://cloud.githubusercontent.com/assets/194400/10266518/ce0c2ba6-6a61-11e5-89bc-4abf33b30f21.png)
+
+![register-3of4](https://cloud.githubusercontent.com/assets/194400/10266523/680d1922-6a62-11e5-9533-3560a646dfd0.png)
+
+We also use https://github.com/chriso/validator.js
+to mitigate [Cross Site Scripting](https://en.wikipedia.org/wiki/Cross-site_scripting)
+vulnerability:
+
+![register-hack-1of2](https://cloud.githubusercontent.com/assets/194400/10267320/5dd3bad6-6a87-11e5-888b-f1e1dbbf9f39.png)
+
+And display a welcome message on successful registration:
+![reg-success-1of2](https://cloud.githubusercontent.com/assets/194400/10267355/c7d8a31e-6a88-11e5-8bf9-3bb148e2d870.png)
+
+## Conclusion
+
+We feel that re-using the handler function as the `failAction`
+keeps the code related to this route/action in a single place
+whereas `server.ext('onPreResponse' ...` will introduce "*hooks*"
+which *can* be a source of confusion (*once an app has many such hooks...*)
+
+## #YMMV
+
+Let us know what you think! [![Join the chat at https://gitter.im/dwyl/chat](https://badges.gitter.im/Join%20Chat.svg)](https://gitter.im/dwyl/chat/?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge&utm_content=badge)
---
+++ server.js
@@ -0,0 +1,223 @@
+//{{{ the opensource libs under the project
+var assert = require('assert');
+var Hapi   = require('hapi');
+var Joi    = require('joi');
+var Vision = require('vision');
+var server = new Hapi.Server({ debug: {"request": ["error", "uncaught"]} })
+var validator = require('validator'); // github.com/chriso/validator.js
+
+//}}}
+//{{{ jos handlers
+var register_fields = {
+  name  : Joi.string().required(),
+  email : Joi.string().email().required()
+};
+//}}}
+//{{{ error handler using jos
+/**
+ * extract_validation_error does what its name suggests
+ * given that the error is not in a very useable format we
+ * need to extract it into a simple set of key:value pairs
+ * @param {Object} error see: http://git.io/vcwiU
+ * @returns {Object} err - the simplified error object
+ */
+function extract_validation_error(error){
+  var key = error.data.details[0].path;
+  err = {}
+  err[key] = {
+    class   : 'input-error',                // css class
+    message : error.data.details[0].message // Joi error message
+  }
+  return err;
+}
+//}}}
+//{{{ the return errors which pop up in use cases
+/**
+ * return_values extracts the values the person submitted if they
+ * submitted the form with incomplete or invalid data so that
+ * the form is not "wiped" each time it gets valdiated!
+ * @param {Object} error - see: http://git.io/vciZd
+ * @returns {Object} values - key:value pairs of the fields
+ * with the value sent by the client.
+ */
+function return_form_input_values(error) {
+  var values = {};
+  var keys = Object.keys(error.data._object)
+  keys.forEach(function(k){
+    values[k] = validator.escape(error.data._object[k]);
+  });
+  return values;
+}
+//}}}
+//{{{ map handler to plot the points
+/**
+ * register_handler is a dual-purpose handler that initially renders
+ * the registration form but is re-used to display the form with any
+ * Joi validation errors to the client until they input valid info
+ * @param {Object} request - the hapi request object
+ * @param {Object} reply - the standard hapi reply object
+ * @param {String} source - source of the invalid field e.g: 'payload'
+ * @param {Object} error - the error object prepared for the client
+ * response (including the validation function error under error.data
+ */
+
+function map_handler(request , reply , source , error) {
+// show the map canvas and all the functions on the map.
+ if( !request.payload || request.payload && error){
+   var errors,values;
+   if(error && error.data){
+     errors = extract_validation_error(error);
+     values = return_form_input_values(error);
+   }
+   return reply.view('map',{
+     title :'Map'+request.server.version,
+     error : errors,
+     values : values
+   	  }).code( error ? 400 : 200);
+   }
+   else {
+   	 return reply.view( 'register_vehicle',{
+		 map : validator.escape(request.payload.map),
+	 })
+ }
+}
+//}}}
+//{{{ registration of vehicles into the system
+function register_vehicle_handler(request , reply , source , error) {
+// show the map canvas and all the functions on the map.
+ if( !request.payload || request.payload && error){
+   var errors,values;
+   if(error && error.data){
+     errors = extract_validation_error(error);
+     values = return_form_input_values(error);
+   }
+   return reply.view('register_vehicle',{
+     title :'Map service:'+request.server.version,
+     error : errors,
+     values : values
+   	  }).code( error ? 400 : 200);
+   }
+   else {
+   	 return reply.view( 'register_vehicle',{
+		 name : validator.escape(request.payload.name),
+		 code : validator.escape(request.payload.alphanumric),
+		 imei : validator.escape(request.payload.name),
+		 phone: validator.escape(request.payload.name)
+	 })
+ }
+}
+//}}}
+//{{{ registration of driver into the system
+function register_driver_handler(request , reply , source , error) {
+// show the map canvas and all the functions on the map.
+ if( !request.payload || request.payload && error){
+   var errors,values;
+   if(error && error.data){
+     errors = extract_validation_error(error);
+     values = return_form_input_values(error);
+   }
+   return reply.view('register_vehicle',{
+     title :'Map service:'+request.server.version,
+     error : errors,
+     values : values
+   	  }).code( error ? 400 : 200);
+   }
+   else {
+   	 return reply.view( 'register_driver',{
+		 name : validator.escape(request.payload.name),
+		 code : validator.escape(request.payload.alphanumric),
+		 imei : validator.escape(request.payload.name),
+		 phone: validator.escape(request.payload.name)
+	 })
+ }
+}
+//}}}
+//{{{ simple register handler to register into the system
+function register_handler(request, reply, source, error) {
+  // show the registration form until its submitted correctly
+  if(!request.payload || request.payload && error) {
+    var errors, values; // return empty if not set.
+    if(error && error.data) { // means the handler is dual-purpose
+      errors = extract_validation_error(error); // the error field + message
+      values = return_form_input_values(error); // avoid wiping form data
+    }
+    return reply.view('registration-form', {
+      title  : 'Please Register to optana :' + request.server.version,
+      error  : errors, // error object used in html template
+      values : values  // (escaped) values displayed in form inputs
+    }).code(error ? 400 : 200); // HTTP status code depending on error
+  }
+  else { // once successful, show welcome message!
+    return reply.view('welcome-message', {
+      name   : validator.escape(request.payload.name),
+      email  : validator.escape(request.payload.email)
+    })
+  }
+}
+//}}}
+//{{{ vechile handler for the system
+function vac_handler(request, reply, source, error){
+   var errors, values;
+   if(error && error.data)
+   if(!request.payload || request.payload && error) {
+     errors = extract_validation_error(error);
+     values = return_form_input_valuse(error);
+   }
+   return reply.view('vac',{
+    title   : 'Short Gist of data :'+request.server.version,
+    error   : errors,
+    values  : valuses
+   }).code(error ? 400 : 200);
+
+}
+//}}}
+//{{{ server connection and the routes which are defined above
+server.connection({ port: process.env.PORT || 8000 });
+server.register(Vision, function (err) {
+  assert(!err, 'Failed to load plugin: ', err); // FATAL ERROR!
+
+  server.views({
+      engines: { html: require('handlebars') },
+      path: __dirname +'/'
+  });
+
+  server.route([{
+    method: 'GET',
+    path: '/',
+    handler: register_handler
+  },{
+     method: 'GET',
+     path: '/vac',
+     handler: vac_handler
+  },{
+    method: 'GET' ,
+    path: '/vcr',
+    handler: register_vehicle_handler
+  },{
+    method: 'GET' ,
+    path: '/vdr',
+    handler: register_driver_handler
+  },{
+    method: 'GET',
+    path: '/map',
+    handler: map_handler
+  },{
+    method: '*',
+    path: '/register',
+    config: {
+      validate: {
+        payload : register_fields,
+        failAction: register_handler
+      }
+    },
+    handler: register_handler
+  }]);
+});
+//}}}
+//{{{ starting the server
+server.start(function() {
+  console.log('Visit: http://127.0.0.1:'+server.info.port);
+});
+
+module.exports = server;
+//}}}
